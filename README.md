a38632cb3940911895a288cc0f24c0a134479db7
Alexander Heinze <git@aheinze.de>
2016-07-21 20:29:01 +0200

    Reformatting, Humidity removed, JSONPath added to extract temperature from json and multiplier to modify values

--- README.md
+++ README.md
@@ -1,40 +1,58 @@
-# homebridge-http-temperature-humidity
+# homebridge-http-temperature
 
-Supports https devices on HomeBridge Platform
+Supports https temperature devices with JSON response on HomeBridge Platform.
+Its a modification of https://github.com/lucacri/homebridge-http-temperature-humidity
 
 # Installation
 
 1. Install homebridge using: npm install -g homebridge
-2. Install this plugin using: npm install -g homebridge-httptemperaturehumidity
+2. Install this plugin using: npm install -g git+https://github.com/alex224/homebridge-http-temperature-humidity.git
 3. Update your configuration file. See sample-config.json in this repository for a sample. 
 
 # Configuration
-
 
 Configuration sample file:
 
  ```
 "accessories": [
-        "accessories": [
         {
             "accessory": "HttpTemphum",
-            "name": "Living Room Weather",
-            "url": "http://192.168.1.210/weather",
-            "sendimmediately": "",
-            "http_method": "GET"
+            "name": "Temperatur Keller",
+            "url": "http://raspberrypi:5984/temps/_all_docs?limit=1&descending=true&include_docs=true",
+            "http_method": "GET",
+            "temperaturePath": "rows[0].doc.Keller",
+            "temperatureMultiplier" : 0.001
         }
     ]
 
 ```
 
 
-The /weather endpoint will return a json looking like this
+In the above example the endpoint returns a json looking like this
 ```
 {
-	"temperature": 25.8,
-	"humidity": 38
+    total_rows: 393549,
+    offset: 0,
+    rows: [
+        {
+            id: "2016-07-21-20-00-02-315789138",
+            key: "2016-07-21-20-00-02-315789138",
+            value: {
+                rev: "1-ae45136d99ed9234284c98871e00421b"
+            },
+            doc: {
+                _id: "2016-07-21-20-00-02-315789138",
+                _rev: "1-ae45136d99ed9234284c98871e00421b",
+                time: "16-07-21 20:00:02",
+                Keller: 20812,
+                Studio: 24500,
+                AussenSZ: 21437
+            }
+        }
+    ]
 }
 ```
+You can handle every json response by changing the temperaturePath to a valid JSONPath expression.
+A reference and examples can be found at http://goessner.net/articles/JsonPath/
 
-
-This plugin acts as an interface between a web endpoint and homebridge only. You will still need some dedicated hardware to expose the web endpoints with the temperature and humidity information. In my case, I used a simple NodeMCU board and a DHT11 (or DHT22).
+This plugin acts as an interface between a web endpoint and homebridge only. You will still need some dedicated hardware to expose the web endpoints with the temperature information. In my case, I used a CouchDB.
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var Service, Characteristic;
 var request = require('sync-request');
+var JSONPath = require('JSONPath');
 
 var temperatureService;
 var humidityService;
-var url 
+var url
 var humidity = 0;
 var temperature = 0;
 
@@ -12,7 +13,6 @@ module.exports = function (homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     homebridge.registerAccessory("homebridge-httptemperaturehumidity", "HttpTemphum", HttpTemphum);
 }
-
 
 function HttpTemphum(log, config) {
     this.log = log;
@@ -25,48 +25,55 @@ function HttpTemphum(log, config) {
     this.manufacturer = config["manufacturer"] || "Luca Manufacturer";
     this.model = config["model"] || "Luca Model";
     this.serial = config["serial"] || "Luca Serial";
+    this.temperaturePath = config["temperaturePath"];
+    this.temperatureMultiplier = config["temperatureMultiplier"] || 1.0;
 }
 
 HttpTemphum.prototype = {
 
     httpRequest: function (url, body, method, username, password, sendimmediately, callback) {
         request({
-                    url: url,
-                    body: body,
-                    method: method,
-                    rejectUnauthorized: false
-                },
-                function (error, response, body) {
-                    callback(error, response, body)
-                })
+            url: url,
+            body: body,
+            method: method,
+            rejectUnauthorized: false
+        },
+            function (error, response, body) {
+                callback(error, response, body)
+            })
     },
 
-    getStateHumidity: function(callback){    
-	callback(null, this.humidity);
+    getStateHumidity: function (callback) {
+        callback(null, this.humidity);
     },
 
     getState: function (callback) {
         var body;
 
-	var res = request(this.http_method, this.url, {});
-	if(res.statusCode > 400){
-	  this.log('HTTP power function failed');
-	  callback(error);
-	} else {
-	  this.log('HTTP power function succeeded!');
-          var info = JSON.parse(res.body);
+        var res = request(this.http_method, this.url, {});
+        if (res.statusCode > 400) {
+            this.log('HTTP request failed');
+            callback(error);
+        } else {
+            this.log('HTTP request succeeded!');
 
-          temperatureService.setCharacteristic(Characteristic.CurrentTemperature, info.temperature);
-          humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, info.humidity);
+            var info = JSON.parse(res.body);
+            this.log(info);
 
-          this.log(res.body);
-          this.log(info);
-
-          this.temperature = info.temperature;
-          this.humidity = info.humidity;
-
-	  callback(null, this.temperature);
-	}
+            var me = this;
+            if (this.temperaturePath) {
+                JSONPath({
+                    json: info, path: this.temperaturePath, callback: function (value) {
+                        me.log("Parse-Result: " + value);
+                        var temperature =  me.temperatureMultiplier * value;
+                        me.log("Format-Result: " + temperature);
+                        temperatureService.setCharacteristic(Characteristic.CurrentTemperature, temperature);
+                        callback(null, temperature);
+                        me.temperature = temperature;
+                    }
+                });
+            }
+        }
     },
 
     identify: function (callback) {
@@ -77,20 +84,15 @@ HttpTemphum.prototype = {
     getServices: function () {
         var informationService = new Service.AccessoryInformation();
         informationService
-                .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-                .setCharacteristic(Characteristic.Model, this.model)
-                .setCharacteristic(Characteristic.SerialNumber, this.serial);
+            .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+            .setCharacteristic(Characteristic.Model, this.model)
+            .setCharacteristic(Characteristic.SerialNumber, this.serial);
 
         temperatureService = new Service.TemperatureSensor(this.name);
         temperatureService
-                .getCharacteristic(Characteristic.CurrentTemperature)
-                .on('get', this.getState.bind(this));
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getState.bind(this));
 
-        humidityService = new Service.HumiditySensor(this.name);
-        humidityService
-                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-                .on('get', this.getStateHumidity.bind(this));
-
-        return [informationService, temperatureService, humidityService];
+        return [informationService, temperatureService];
     }
 };
